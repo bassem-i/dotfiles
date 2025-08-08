322ca054bf1b1728f6d581c2afb2a25aeb8272fc
Bassem Ibrahim <17043634+BassemMohamed@users.noreply.github.com>
2025-08-08 23:00:43 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,11 +6,13 @@ brew install zsh
 brew install wezterm
 brew install tmux
 brew install neovim
+brew install ripgrep
+brew install fzf
 brew install eza
 brew install llm
 brew install stow
+brew install node
 
 # Install zsh-autosuggestions & zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
